1fb218b2ce67b7156a8b4d1c8e41dac80c4b2aae
gr33dywolf <quentin.suply@outlook.fr>
2025-11-15 00:55:20 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,12 +1,10 @@
 from flask import Flask
 
-app = Flask("keep_alive")
+app = Flask('keep_alive')
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Bot Vinted actif."
+    return "OK", 200
 
-def keep_alive():
-    from threading import Thread
-    t = Thread(target=lambda: app.run(host="0.0.0.0", port=8080))
-    t.start()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
