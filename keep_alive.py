b052cd211c973c19806a172186b449a665cd5fdf
gr33dywolf <quentin.suply@outlook.fr>
2025-11-15 23:04:38 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,20 +1,14 @@
-# keep_alive.py
 from flask import Flask
 import threading
 import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.get("/")
 def home():
-    return "vtd_scanner alive", 200
+    return "OK", 200
 
-def run():
-    port = int(os.getenv("PORT", "8080"))
+
+def start_server():
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
-
-def start():
-    t = threading.Thread(target=run, daemon=True)
-    t.start()
-
-start()
